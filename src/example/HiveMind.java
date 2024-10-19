b9c514ef2d1f3ac4586d5cdc952e9c014670d6aa
FallingDice <songinsuak@gmail.com>
2024-10-19 21:38:54 +0900

    Update and rename ExampleJavaMod.java to HiveMind.java

---
+++ src/example/HiveMind.java
@@ -0,0 +1,24 @@
+package hivemind;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+import hivemind.content.*;
+
+public class HiveMind extends Mod{
+
+    public HiveMind(){
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+
+}
