635a8c9a3c30dd454d2f08a805b0cb10c517a722
FallingDice <songinsuak@gmail.com>
2024-10-19 22:18:47 +0900

    Update HiveMind.java

--- src/hivemind/HiveMind.java
+++ src/hivemind/HiveMind.java
@@ -14,10 +14,12 @@ import hivemind.content.*;
 public class HiveMind extends Mod{
 
     public HiveMind(){
+        //모드 초기화 코드
     }
 
     @Override
     public void loadContent(){
+        //콘텐츠 로드 코드
         HiveMindUnitTypes.load();
     }
 
